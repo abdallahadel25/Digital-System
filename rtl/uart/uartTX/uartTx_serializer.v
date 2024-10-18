585dcf44c69fc57c1177181889db038f508974a3
abdallahadel25 <abdallahadel25@gmail.com>
2024-10-18 22:15:38 +0300

    uartTx p_data isolation

--- rtl/uart/uartTX/uartTx_serializer.v
+++ rtl/uart/uartTX/uartTx_serializer.v
@@ -3,34 +3,35 @@ parameter dataWidth = 8,
 parameter counterWidth = 3
 )(
 input 	wire [dataWidth-1:0] 	p_data,
-input 	wire 			ser_en,
-input	wire			clk,
-input	wire			rst,
-output 	reg			ser_data,
-output	wire			ser_done
+input 	wire 					ser_en,
+input	wire					clk,
+input	wire					rst,
+output 	wire					ser_data,
+output	wire					ser_done
 );
 
-reg [counterWidth-1:0] 	counter; 
+reg [counterWidth-1:0] 	counter;
+reg [dataWidth-1:0]		data_v;
 
 assign ser_done = (counter == 0);
+assign ser_data = data_v[0];
 
 always@(posedge clk or negedge rst)
 begin
 	if(!rst)
 	begin
-		ser_data <= 1'b0;
+		data_v <= {dataWidth{1'b0}};
 		counter <= {counterWidth{1'b0}};
 	end
-
 	else if (counter != 0)
 	begin
-		ser_data <= p_data[dataWidth-counter];
+		data_v <= data_v >> 1;
 		counter <= counter - 1;
 	end
 	else if (ser_en)
 	begin
+		data_v <= p_data;
 		counter <= dataWidth - 1;
-		ser_data <= p_data[0];
 	end
 end
 endmodule
