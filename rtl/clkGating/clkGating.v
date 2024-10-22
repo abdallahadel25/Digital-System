af49b819e5c9bb698c8ceb5415541f14f6843427
abdallahadel25 <abdallahadel25@gmail.com>
2024-10-22 14:36:06 +0300

    clkGating

---
+++ rtl/clkGating/clkGating.v
@@ -0,0 +1,27 @@
+module CLK_GATE (
+input      clk_en,
+input      clk,
+output     gated_clk
+);
+
+reg latch_out;
+
+always @(clk or clk_en)
+begin
+    if (!clk)
+        latch_out <= clk_en
+end
+
+assign gated_clk = clk & latch_out;
+
+/*
+
+TLATNCAX12M U0_TLATNCAX12M (
+.E(CLK_EN),
+.CK(CLK),
+.ECK(GATED_CLK)
+);
+
+*/
+
+endmodule
